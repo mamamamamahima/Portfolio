fbe56f3aad6945baba96c2c1a8219a5a8de528aa
mamamamamahima <57428772+mamamamamahima@users.noreply.github.com>
2020-04-19 01:10:58 -0700

    Added all files from repl.it portfolio

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p> Here are all of my Links [Mahima Iyer, CSP7]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <!-- Links to all of the website's different pages-->
+      <li><a href="Last_AboutMe.html">AboutMe</a></li></br>
+      <li><a href="Last_Projects.html">Projects</a></li></br>
+      <li><a href="Last_Resume.html">Resume</a></li></br>
+      <li><a href="Last_ContactMe.html">ContactMe</a></li></br>
+    <ul>
+  </body>
+</html>
